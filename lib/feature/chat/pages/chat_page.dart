308ccf19f5d791f2d5aaf733e00770a83d2cc4c4
auroramiri <auroramiria000@gmail.com>
2025-03-20 10:26:21 +0300

    build Chat pages beautiful UI and a basic text functionality

--- lib/feature/chat/pages/chat_page.dart
+++ lib/feature/chat/pages/chat_page.dart
@@ -1,31 +1,45 @@
+import 'dart:math';
+
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:custom_clippers/custom_clippers.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:message_app/common/helper/last_seen_message.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:message_app/common/extension/custom_theme_extension.dart';
 import 'package:message_app/common/models/user_model.dart';
 import 'package:message_app/common/routes/routes.dart';
 import 'package:message_app/common/widgets/custom_icon_button.dart';
 import 'package:message_app/feature/auth/controller/auth_controller.dart';
+import 'package:message_app/feature/chat/controller/chat_controller.dart';
+import 'package:message_app/feature/chat/widgets/chat_text_field.dart';
+import 'package:message_app/feature/chat/widgets/message_card.dart';
+import 'package:message_app/feature/chat/widgets/show_date_card.dart';
+import 'package:message_app/feature/chat/widgets/yellow_card.dart';
+
+import '../../../common/helper/last_seen_message.dart';
+
+final pageStorageBucket = PageStorageBucket();
 
 class ChatPage extends ConsumerWidget {
-  const ChatPage({super.key, required this.user});
+  ChatPage({super.key, required this.user});
 
   final UserModel user;
-
-  
+  final ScrollController scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
+      backgroundColor: context.theme.chatPageBgColor,
       appBar: AppBar(
         leading: InkWell(
           onTap: () {
-            Navigator.of(context).pop();
+            Navigator.pop(context);
           },
           borderRadius: BorderRadius.circular(20),
           child: Row(
             children: [
-              Icon(Icons.arrow_back),
+              const Icon(Icons.arrow_back),
               Hero(
                 tag: 'profile',
                 child: Container(
@@ -52,27 +66,28 @@ class ChatPage extends ConsumerWidget {
               children: [
                 Text(
                   user.username,
-                  style: TextStyle(fontSize: 18, color: Colors.white),
+                  style: const TextStyle(fontSize: 18, color: Colors.white),
                 ),
-                SizedBox(height: 3),
+                const SizedBox(height: 3),
                 StreamBuilder(
                   stream: ref
                       .read(authControllerProvider)
                       .getUserPrecenceStatus(uid: user.uid),
                   builder: (_, snapshot) {
                     if (snapshot.connectionState != ConnectionState.active) {
-                      return Text(
-                        'connecting...',
-                        style: TextStyle(fontSize: 12, color: Colors.white),
-                      );
+                      return const SizedBox();
                     }
+
                     final singleUserModel = snapshot.data!;
-            
-                    final lastMessage = lastSeenMessage(singleUserModel.lastSeen);
-            
+                    final lastMessage = lastSeenMessage(
+                      singleUserModel.lastSeen,
+                    );
+
                     return Text(
-                      singleUserModel.active ? "Online" : "$lastMessage ago",
-                      style: TextStyle(fontSize: 12, color: Colors.white),
+                      singleUserModel.active
+                          ? 'online'
+                          : "last seen $lastMessage ago",
+                      style: const TextStyle(fontSize: 12, color: Colors.white),
                     );
                   },
                 ),
@@ -83,18 +98,154 @@ class ChatPage extends ConsumerWidget {
         actions: [
           CustomIconButton(
             onPressed: () {},
-            icon: Icons.call,
+            icon: Icons.video_call,
             iconColor: Colors.white,
           ),
           CustomIconButton(
             onPressed: () {},
-            icon: Icons.video_call,
+            icon: Icons.call,
             iconColor: Colors.white,
           ),
           CustomIconButton(
             onPressed: () {},
             icon: Icons.more_vert,
             iconColor: Colors.white,
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          // chat background image
+          Image(
+            height: double.maxFinite,
+            width: double.maxFinite,
+            image: const AssetImage('assets/images/doodle_bg.png'),
+            fit: BoxFit.cover,
+            color: context.theme.chatPageDoodleColor,
+          ),
+          // Stream of Chat
+          Padding(
+            padding: const EdgeInsets.only(bottom: 60),
+            child: StreamBuilder(
+              stream: ref
+                  .watch(chatControllerProvider)
+                  .getAllOneToOneMessage(user.uid),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState != ConnectionState.active) {
+                  return ListView.builder(
+                    itemCount: 15,
+                    itemBuilder: (_, index) {
+                      final random = Random().nextInt(14);
+                      return Container(
+                        alignment:
+                            random.isEven
+                                ? Alignment.centerRight
+                                : Alignment.centerLeft,
+                        margin: EdgeInsets.only(
+                          top: 5,
+                          bottom: 5,
+                          left: random.isEven ? 150 : 15,
+                          right: random.isEven ? 15 : 150,
+                        ),
+                        child: ClipPath(
+                          clipper: UpperNipMessageClipperTwo(
+                            random.isEven
+                                ? MessageType.send
+                                : MessageType.receive,
+                            nipWidth: 8,
+                            nipHeight: 10,
+                            bubbleRadius: 12,
+                          ),
+                          child: Shimmer.fromColors(
+                            baseColor:
+                                random.isEven
+                                    ? context.theme.greyColor!.withValues(
+                                      alpha: 0.3,
+                                    )
+                                    : context.theme.greyColor!.withValues(
+                                      alpha: 0.2,
+                                    ),
+                            highlightColor:
+                                random.isEven
+                                    ? context.theme.greyColor!.withValues(
+                                      alpha: 0.4,
+                                    )
+                                    : context.theme.greyColor!.withValues(
+                                      alpha: 0.3,
+                                    ),
+                            child: Container(
+                              height: 40,
+                              width:
+                                  170 + double.parse((random * 2).toString()),
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }
+
+                return PageStorage(
+                  bucket: pageStorageBucket,
+                  child: ListView.builder(
+                    key: const PageStorageKey('chat_page_list'),
+                    itemCount: snapshot.data!.length,
+                    shrinkWrap: true,
+                    controller: scrollController,
+                    itemBuilder: (_, index) {
+                      final message = snapshot.data![index];
+                      final isSender =
+                          message.senderId ==
+                          FirebaseAuth.instance.currentUser!.uid;
+
+                      final haveNip =
+                          (index == 0) ||
+                          (index == snapshot.data!.length - 1 &&
+                              message.senderId !=
+                                  snapshot.data![index - 1].senderId) ||
+                          (message.senderId !=
+                                  snapshot.data![index - 1].senderId &&
+                              message.senderId ==
+                                  snapshot.data![index + 1].senderId) ||
+                          (message.senderId !=
+                                  snapshot.data![index - 1].senderId &&
+                              message.senderId !=
+                                  snapshot.data![index + 1].senderId);
+                      final isShowDateCard =
+                          (index == 0) ||
+                          ((index == snapshot.data!.length - 1) &&
+                              (message.timeSent.day >
+                                  snapshot.data![index - 1].timeSent.day)) ||
+                          (message.timeSent.day >
+                                  snapshot.data![index - 1].timeSent.day &&
+                              message.timeSent.day <=
+                                  snapshot.data![index + 1].timeSent.day);
+
+                      return Column(
+                        children: [
+                          if (index == 0) const YellowCard(),
+                          if (isShowDateCard)
+                            ShowDateCard(date: message.timeSent),
+                          MessageCard(
+                            isSender: isSender,
+                            haveNip: haveNip,
+                            message: message,
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+          Container(
+            alignment: const Alignment(0, 1),
+            child: ChatTextField(
+              receiverId: user.uid,
+              scrollController: scrollController,
+            ),
           ),
         ],
       ),
--- lib/feature/home/pages/chat_home_page.dart
+++ lib/feature/home/pages/chat_home_page.dart
@@ -1,20 +1,87 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
+import 'package:message_app/common/extension/custom_theme_extension.dart';
+import 'package:message_app/common/models/last_message_model.dart';
+import 'package:message_app/common/models/user_model.dart';
 import 'package:message_app/common/routes/routes.dart';
+import 'package:message_app/common/utils/coloors.dart';
+import 'package:message_app/feature/chat/controller/chat_controller.dart';
 
-class ChatHomePage extends StatelessWidget {
+class ChatHomePage extends ConsumerWidget {
   const ChatHomePage({super.key});
 
-  navigateToContactPage(context) {
-    Navigator.pushNamed(context, Routes.contact);
-  }
-
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Center(child: Text('Chat Home Page')),
+      body: StreamBuilder<List<LastMessageModel>>(
+        stream: ref.watch(chatControllerProvider).getAllLastMessageList(),
+        builder: (_, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(color: Coloors.blueDark),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              final lastMessageData = snapshot.data![index];
+              return ListTile(
+                onTap: () {
+                  Navigator.pushNamed(
+                    context,
+                    Routes.chat,
+                    arguments: UserModel(
+                      username: lastMessageData.username,
+                      uid: lastMessageData.contactId,
+                      profileImageUrl: lastMessageData.profileImageUrl,
+                      active: true,
+                      lastSeen: 0,
+                      phoneNumber: '0',
+                      groupId: [],
+                    ),
+                  );
+                },
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(lastMessageData.username),
+                    Text(
+                      DateFormat.Hm().format(lastMessageData.timeSent),
+                      style: TextStyle(
+                        fontSize: 13,
+                        color: context.theme.greyColor,
+                      ),
+                    ),
+                  ],
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 3),
+                  child: Text(
+                    lastMessageData.lastMessage,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(color: context.theme.greyColor),
+                  ),
+                ),
+                leading: CircleAvatar(
+                  backgroundImage: CachedNetworkImageProvider(
+                    lastMessageData.profileImageUrl,
+                  ),
+                  radius: 24,
+                ),
+              );
+            },
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => navigateToContactPage(context),
-        child: Icon(Icons.chat),
+        onPressed: () {
+          Navigator.pushNamed(context, Routes.contact);
+        },
+        child: const Icon(Icons.chat),
       ),
     );
   }
